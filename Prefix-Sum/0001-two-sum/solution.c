f39d71213bd94b033f2c30a9601a0582029c7b74
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2025-06-30 13:18:54 +0000

    [LeetCode Sync] Runtime - 100 ms (53.04%), Memory - 8.6 MB (99.97%)

--- Prefix-Sum/0001-two-sum/solution.c
+++ Prefix-Sum/0001-two-sum/solution.c
@@ -3,17 +3,17 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* result = (int*)malloc(2 * sizeof(int));
-    int sum = 0;
-    for(int i = 0; i < numsSize; i += 1){
+     *returnSize = 2;
+    for(int i = 0; i < numsSize - 1; i += 1){
         for(int j = i + 1; j < numsSize; j += 1){
             if(nums[i] + nums[j] == target){
-                *returnSize = 2;
+               
                 result[0] = i;
                 result[1] = j;
                 return result;
             } 
         }
     }
-    *returnSize = 0;
+    // *returnSize = 0;
     return 0;
 }
