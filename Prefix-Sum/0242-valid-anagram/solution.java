72748bd02e2d97ed54464b0b5d7907df22d1e1cc
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2025-07-03 09:44:55 +0000

    [LeetCode Sync] Runtime - 4 ms (83.69%), Memory - 45 MB (50.66%)

---
+++ Prefix-Sum/0242-valid-anagram/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] string1 = s.toCharArray();
+        char[] string2 = t.toCharArray();
+
+        Arrays.sort(string1);
+        Arrays.sort(string2);
+
+        return Arrays.equals(string1, string2);
+    }
+}
+
+//Time Complexity : O(N log N)
+//Space Complexity : O(N)
