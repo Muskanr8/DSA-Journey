de6cc908a3c0fc894a7cf84c2fc2d016275387ac
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2025-07-14 16:15:35 +0000

    [LeetCode Sync] Runtime - 4 ms (83.69%), Memory - 43.1 MB (99.97%)

--- Prefix-Sum/0242-valid-anagram/solution.java
+++ Prefix-Sum/0242-valid-anagram/solution.java
@@ -1,14 +1,32 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] string1 = s.toCharArray();
-        char[] string2 = t.toCharArray();
-
-        Arrays.sort(string1);
-        Arrays.sort(string2);
-
-        return Arrays.equals(string1, string2);
+        int[] freq = new int[26];
+        int slen = s.length();
+        int tlen = t.length();
+        if(slen != tlen){
+            return false;
+        }
+        for(int i = 0; i < slen; i += 1){
+            char ch = s.charAt(i);
+            char ch1 = t.charAt(i);
+            freq[ch - 'a'] += 1;
+            freq[ch1 - 'a'] -= 1;
+        }
+        for(int i = 0; i < 26; i += 1){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
+        // char[] string1 = s.toCharArray();
+        // char[] string2 = t.toCharArray();
+
+        // Arrays.sort(string1);
+        // Arrays.sort(string2);
+
+        // return Arrays.equals(string1, string2);
 
 //Time Complexity : O(N log N)
 //Space Complexity : O(N)
