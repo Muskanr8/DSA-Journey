8c7af907d60581c5c820cf6c6bf022723e029238
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2026-06-15 07:32:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (23.92%)

--- Prefix-Sum/0027-remove-element/solution.java
+++ Prefix-Sum/0027-remove-element/solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
-        int k = 0;
-        for(int i = 0; i < nums.length; i += 1){
-            if(nums[i] != val){
-                nums[k] = nums[i];
-                k += 1;
+    public int removeElement(int[] arr, int val) {
+        int i = 0, n = arr.length;
+        for(int j = 0; j < n; j += 1){
+            if(arr[j] != val){
+                arr[i] = arr[j];
+                i += 1;
             }
         }
-        return k;
+        return i;
     }
 }
