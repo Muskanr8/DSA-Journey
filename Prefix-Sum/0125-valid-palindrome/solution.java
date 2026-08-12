e207de960d0d03b597999d36132fe21a903f5aac
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2026-08-12 09:18:59 +0000

    [LeetCode Sync] Runtime - 3 ms (56.05%), Memory - 44.1 MB (82.71%)

--- Prefix-Sum/0125-valid-palindrome/solution.java
+++ Prefix-Sum/0125-valid-palindrome/solution.java
@@ -1,26 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
+
         s = s.toLowerCase();
-        if (s.isEmpty()) {
+
+        int i = 0, j = s.length() - 1;
+
+        if(s.isEmpty()){
             return true;
         }
-        while (i < j) {
 
-            if (!Character.isLetterOrDigit(s.charAt(i))) {
+        while(i < j){
+            if(!Character.isLetterOrDigit(s.charAt(i))){
                 i++;
-            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(j))){
                 j--;
-            } else {
-
-                if (s.charAt(i) != s.charAt(j)) {
+            }
+            else{
+                if(s.charAt(i) != s.charAt(j)){
                     return false;
                 }
-
                 i++;
                 j--;
             }
         }
+
         return true;
     }
 }
