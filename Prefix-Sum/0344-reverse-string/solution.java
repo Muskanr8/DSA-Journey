9ba329d5333869c44f4bf3dca742acb087180464
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2026-08-12 08:39:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48 MB (93.89%)

--- Prefix-Sum/0344-reverse-string/solution.java
+++ Prefix-Sum/0344-reverse-string/solution.java
@@ -5,8 +5,9 @@ class Solution {
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
-            i += 1;
-            j -= 1;
+            
+            i++;
+            j--;
         }
     }
 }
