e28450ffa187b8083615de93e6b8896f8587be70
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2026-08-13 08:11:40 +0000

    [LeetCode Sync] Runtime - 2 ms (99.01%), Memory - 47 MB (13.03%)

--- Prefix-Sum/0345-reverse-vowels-of-a-string/solution.java
+++ Prefix-Sum/0345-reverse-vowels-of-a-string/solution.java
@@ -1,29 +1,45 @@
 class Solution {
-    private boolean isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' ||
-                ch == 'o' || ch == 'u' ||
-                ch == 'A' || ch == 'E' || ch == 'I' ||
-                ch == 'O' || ch == 'U';
-    }
-
-
     public String reverseVowels(String s) {
-        char[] arr = s.toCharArray();
-        int i = 0, j = arr.length - 1;
-        while(i < j){
-            if(!isVowel(arr[i]))
+        char[] str = s.toCharArray();
+        int i = 0, j = s.length()-1;
+
+        while(i <= j){
+            if(str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U' && str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u'){
                 i++;
-            else if(!isVowel(arr[j]))
+            }
+            else if(str[j] != 'A' && str[j] != 'E' && str[j] != 'I' && str[j] != 'O' && str[j] != 'U' && str[j] != 'a' && str[j] != 'e' && str[j] != 'i' && str[j] != 'o' && str[j] != 'u'){
                 j--;
+            }
             else{
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+
                 i++;
                 j--;
             }
         }
-        String str = new String(arr);
-        return str;
+
+        return new String(str);
+        // StringBuilder str = new StringBuilder(s);
+        // s = s.toLowerCase();
+        // while(i <= j){
+        //     if(s.charAt(i) != 'a'&& s.charAt(i) != 'e' && s.charAt(i) != 'i' && s.charAt(i) != 'o' && s.charAt(i) != 'u'){
+        //         i++;
+        //     }
+        //     else if(s.charAt(j) != 'a'&& s.charAt(j) != 'e' && s.charAt(j) != 'i' && s.charAt(j) != 'o' &&  s.charAt(j) != 'u'){
+        //         j--;
+        //     }
+        //     else{
+        //         char ch = str.charAt(i);
+        //         str.setCharAt(i, str.charAt(j));
+        //         str.setCharAt(j, ch);
+
+        //         i++;
+        //         j--;
+        //     }
+        // }
+
+        // return str.toString();
     }
 }
