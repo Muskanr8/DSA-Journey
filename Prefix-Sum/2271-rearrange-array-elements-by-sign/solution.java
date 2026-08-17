d8c28670e924917952e775a0faf8a8b270dfc618
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2026-08-17 14:44:30 +0000

    [LeetCode Sync] Runtime - 3 ms (100.00%), Memory - 175.4 MB (70.21%)

---
+++ Prefix-Sum/2271-rearrange-array-elements-by-sign/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int i = 0, j = 1;
+        for(int num : nums){
+            if(num > 0){
+                ans[i] = num;
+                i += 2;
+            }
+            else{
+                ans[j] = num;
+                j += 2;
+            }
+        }
+
+        return ans;
+    }
+}
