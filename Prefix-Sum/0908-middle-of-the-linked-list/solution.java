c5800ec5496be18caa0ab2df01044337277677b9
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2026-08-22 10:45:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.8 MB (53.04%)

--- Prefix-Sum/0908-middle-of-the-linked-list/solution.java
+++ Prefix-Sum/0908-middle-of-the-linked-list/solution.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+        ListNode fast = head, slow = head;
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
