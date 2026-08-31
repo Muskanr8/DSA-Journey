539f2f802fdf0cc275b96549b5fdb9aba2acee81
Muskan Shaik <129413369+Muskanr8@users.noreply.github.com>
2026-08-31 15:05:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.7 MB (63.33%)

--- Prefix-Sum/0088-merge-sorted-array/solution.java
+++ Prefix-Sum/0088-merge-sorted-array/solution.java
@@ -1,35 +1,17 @@
 class Solution {
-    public void merge(int[] nums1, int n, int[] nums2, int m) {
-      int i = 0;
-      int j = 0;
-      int k = 0;
-      int[] res = new int[n + m];
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int i = m - 1, j = n - 1, k = m + n - 1;
 
-      while(i < n && j < m){
-        if(nums1[i] < nums2[j]){
-            res[k] = nums1[i];
-            i += 1;
+        while(j >= 0){
+
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums1[k--] = nums2[j--];
+            }
         }
-        else{
-            res[k] = nums2[j];
-            j += 1;
-        }
-        k += 1;
-      }
-
-      while(i < n){
-        res[k] = nums1[i];
-        i += 1;
-        k += 1;
-      }
-
-      while(j < m){
-        res[k] = nums2[j];
-        j += 1;
-        k += 1;
-      }
-
-      for (int p = 0; p < m + n; p++) 
-        nums1[p] = res[p];
     }
 }
+
